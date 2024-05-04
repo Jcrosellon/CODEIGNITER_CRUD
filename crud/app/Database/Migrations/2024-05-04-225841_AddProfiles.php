ee9dbd40b563e024c2ae6453a351e180d07e7bf9
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-05-04 18:14:42 -0500

    Nuevo cambio
    Se crean dos metodos en migrations

--- crud/app/Database/Migrations/2024-05-04-225841_AddProfiles.php
+++ crud/app/Database/Migrations/2024-05-04-225841_AddProfiles.php
@@ -8,11 +8,42 @@ class AddProfiles extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'BIGINT',
+                'constraint' => 20,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'unique' => true
+            ],
+            'photo' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 30
+            ],
+            'create_at'=>[
+                'type'=>'TIMESTAMP',
+                'null'=>true
+            ],
+            'update_at'=>[
+                'type'=>'TIMESTAMP',
+                'null'=>true
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('profiles');
+        
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('profiles');
     }
 }
