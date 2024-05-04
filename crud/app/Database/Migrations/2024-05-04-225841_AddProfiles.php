7ff9079832dbc5788331924fca4544f65c21ab25
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-05-04 18:00:08 -0500

    Nuevo cambio
    Se crea el archivo migracion de esta manera
    php spark make:migration AddProfiles

---
+++ crud/app/Database/Migrations/2024-05-04-225841_AddProfiles.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddProfiles extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
