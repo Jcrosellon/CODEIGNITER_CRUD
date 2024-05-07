cd5125d15aa13db1ee91cf50762772f697b60a22
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-05-06 23:18:52 -0500

    Nuevo cambio
    se crean las function en Profile.php, se configuran las routes.php y se crea el archivo add_profile.php

--- crud/app/Config/Routes.php
+++ crud/app/Config/Routes.php
@@ -6,3 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+//CRUD RESTful Routes
+$routes->get('profiles-list', 'Profile::index');
+$routes->get('profile-form', 'Profile::create');
+$routes->post('submit-form', 'Profile::store');
+$routes->get('edit-view/(:num)', 'Profile::singleProfile/$1');
+$routes->post('update', 'Profile::update');
+$routes->get('delete/(:num)', 'Profile::delete/$1');
